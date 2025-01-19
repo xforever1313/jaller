edc62beee778f5c14d1aad6200fa81f5809f7c35
Seth Hendrick <seth@shendrick.net>
2025-01-19 14:05:31 -0500

    Add initial commit of the server project.

---
+++ src/Jaller.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jaller.Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
+    }
+}
