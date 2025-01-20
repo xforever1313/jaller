a2586f0fa48a46fbb8f85781b3c64e1fa5c5d3b7
Seth Hendrick <seth@shendrick.net>
2025-01-20 12:21:32 -0500

    Added CID.

---
+++ src/Jaller.Core/Cid.cs
@@ -0,0 +1,85 @@
+﻿//
+// Jaller - An advanced IPFS Gateway
+// Copyright (C) 2025 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+namespace Jaller.Core
+{
+    /// <summary>
+    /// The CID of a file that is hosted.
+    /// </summary>
+    public sealed record class Cid
+    {
+        // ---------------- Constructor ----------------
+
+        private Cid( string version0, string version1 )
+        {
+            this.Version0Cid = version0;
+            this.Version1Cid = version1;
+        }
+
+        // ---------------- Properties ----------------
+
+        /// <summary>
+        /// The CID using the version 0 algorithm.
+        /// 
+        /// Version 0 used base 58-encoded multihashes.
+        /// If the CID is 46 characters and starts with "Qm",
+        /// its most likely version 0.
+        /// </summary>
+        public string Version0Cid { get; }
+
+        /// <summary>
+        /// The CID using the version 1 hashing algorithm.
+        /// 
+        /// This includes:
+        /// - A multibase prefix
+        /// - The CID version identifier
+        /// - A mulitcoded identifier.
+        /// </summary>
+        public string Version1Cid { get; }
+
+        // ---------------- Methods ----------------
+
+        public static Cid Parse( string hash )
+        {
+            Ipfs.Cid innerCid = Ipfs.Cid.Decode( hash );
+
+            string version0;
+            string version1;
+            if( innerCid.Version == 0 )
+            {
+                version0 = hash;
+                innerCid.Version = 1;
+                version1 = innerCid.Encode();
+            }
+            else if( innerCid.Version == 1 )
+            {
+                version1 = hash;
+                innerCid.Version = 0;
+                version0 = innerCid.Encode();
+            }
+            else
+            {
+                throw new FormatException(
+                    "Unknown version for CID: " + innerCid.Version
+                );
+            }
+
+            return new Cid( version0, version1 );
+        }
+    }
+}
---
+++ src/Jaller.Tests/Core/CidTests.cs
@@ -0,0 +1,95 @@
+﻿//
+// Jaller - An advanced IPFS Gateway
+// Copyright (C) 2025 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Jaller.Core;
+
+namespace Jaller.Tests.Core
+{
+    [TestClass]
+    public sealed class CidTests
+    {
+        // ---------------- Tests ----------------
+
+        /// <summary>
+        /// Given various IPFS version 0 hashes,
+        /// does the hash convert between version 0 and version
+        /// 1 correctly?
+        /// </summary>
+        [TestMethod]
+        public void CidVersion0ParseTest()
+        {
+            DoParseV0Test(
+                "Qmaobf4QpYonZL4dGDhoc68ZQ7SfCwRdcC9fU9TJyQnBpY",
+                "bafybeifzgn4th5udmc4u6hnv4b4xeaommqn64g763ifwbc3pa6ihemfx4u" 
+            );
+
+            DoParseV0Test(
+                "QmVXrfQGSbErw7nNGYD2v4VU6dUvdfJPLJaAyzwuFeKSWH",
+                "bafybeidk4c3i3opkvz4unqb6x7ziubdxf7shicazj4zauqfzlplcfdxzay"
+            );
+
+            DoParseV0Test(
+                "QmQXqxzncc26BeKiYXvxDQ283a9m6XJomWcq6kZGo9jtvA",
+                "bafybeibasv62htrvcazopuxuegadvdh7apluyrafxumblulilznjdukcy4"
+            );
+        }
+
+        /// <summary>
+        /// Given various IPFS version 1 hashes,
+        /// does the hash convert between version 1 and version
+        /// 0 correctly?
+        /// </summary>
+        [TestMethod]
+        public void CidVersion1ParseTest()
+        {
+            DoParseV1Test(
+                "Qmaobf4QpYonZL4dGDhoc68ZQ7SfCwRdcC9fU9TJyQnBpY",
+                "bafybeifzgn4th5udmc4u6hnv4b4xeaommqn64g763ifwbc3pa6ihemfx4u"
+            );
+
+            DoParseV1Test(
+                "QmVXrfQGSbErw7nNGYD2v4VU6dUvdfJPLJaAyzwuFeKSWH",
+                "bafybeidk4c3i3opkvz4unqb6x7ziubdxf7shicazj4zauqfzlplcfdxzay"
+            );
+
+            DoParseV1Test(
+                "QmQXqxzncc26BeKiYXvxDQ283a9m6XJomWcq6kZGo9jtvA",
+                "bafybeibasv62htrvcazopuxuegadvdh7apluyrafxumblulilznjdukcy4"
+            );
+        }
+
+        // ---------------- Test Helpers ----------------
+
+        private void DoParseV0Test( string expectedV0, string expectedV1 )
+        {
+            DoParseTest( expectedV0, expectedV0, expectedV1 );
+        }
+
+        private void DoParseV1Test( string expectedV0, string expectedV1 )
+        {
+            DoParseTest( expectedV1, expectedV0, expectedV1 );
+        }
+
+        private void DoParseTest( string uutHash, string expectedV0, string expectedV1 )
+        {
+            var cid = Cid.Parse( uutHash );
+            Assert.AreEqual( expectedV0, cid.Version0Cid );
+            Assert.AreEqual( expectedV1, cid.Version1Cid );
+        }
+    }
+}
