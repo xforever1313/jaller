f9025ed6346ea7c817bceba73e66160c86c02ce6
Seth Hendrick <seth@shendrick.net>
2025-03-07 16:59:43 -0500

    Added contracts classes so client can get folder and file information.

---
+++ src/Jaller.Contracts/FileManagement/JallerFileInfo.cs
@@ -0,0 +1,45 @@
+﻿//
+// Jaller - An advanced IPFS Gateway
+// Copyright (C) 2025 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System.Text.Json.Serialization;
+
+namespace Jaller.Contracts.FileManagement;
+
+public record class JallerFileInfo
+{
+    // ---------------- Properties ----------------
+
+    [JsonRequired]
+    [JsonPropertyName( "cidv1" )]
+    public required string CidV1 { get; init; }
+
+    [JsonPropertyName( "parent_folder" )]
+    public int? ParentFolderId { get; init; }
+
+    [JsonPropertyName( "name" )]
+    public string Name { get; init; } = "Untitled";
+
+    [JsonPropertyName( "description" )]
+    public string? Description { get; init; }
+
+    [JsonPropertyName( "mime_type" )]
+    public string? MimeType { get; init; }
+
+    [JsonPropertyName( "tags" )]
+    public string[]? Tags { get; init; }
+}
---
+++ src/Jaller.Server/Extensions/JallerFileExtensions.cs
@@ -0,0 +1,40 @@
+﻿//
+// Jaller - An advanced IPFS Gateway
+// Copyright (C) 2025 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Jaller.Contracts.FileManagement;
+using Jaller.Standard.FileManagement;
+
+namespace Jaller.Server.Extensions;
+
+public static class JallerFileExtensions
+{
+    // ---------------- Methods ----------------
+
+    public static JallerFileInfo ToFileInfo( this JallerFile file )
+    {
+        return new JallerFileInfo
+        {
+            CidV1 = file.CidV1,
+            Description = file.Description,
+            MimeType = file.MimeType,
+            Name = file.Name,
+            ParentFolderId = file.ParentFolder,
+            Tags = file.Tags?.ToArray() ?? null
+        };
+    }
+}
