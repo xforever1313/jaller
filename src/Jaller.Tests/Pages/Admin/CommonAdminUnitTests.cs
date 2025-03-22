aa1c05316bbe3f2b7b406789c572b6d6a9d730d7
Seth Hendrick <seth@shendrick.net>
2025-03-22 16:07:09 -0400

    Added unit tests to ensure we filter out forbidden hosts.

---
+++ src/Jaller.Tests/Pages/Admin/CommonAdminUnitTests.cs
@@ -0,0 +1,76 @@
+﻿//
+// Jaller - An advanced IPFS Gateway
+// Copyright (C) 2025 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System.Net;
+using Jaller.Core.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Jaller.Tests.Pages.Admin
+{
+    public static class CommonAdminUnitTests
+    {
+        /// <summary>
+        /// Ensures the host is restricted based on the configuration.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="config"></param>
+        public static void DoHostRestrictionTest( Func<IActionResult> action, PageModel model, JallerWebConfig config )
+        {
+            var httpContext = new DefaultHttpContext();
+            model.PageContext = new PageContext
+            {
+                HttpContext = httpContext
+            };
+
+            var urlsToTry = new string[]
+            {
+                "localhost",
+                "ipfs.shendrick.net"
+            };
+
+            // Empty array means admin panel is basically disabled.
+            config.AllowedAdminHosts = [];
+
+            foreach( string url in urlsToTry )
+            {
+                model.Request.Host = new HostString( url );
+
+                IActionResult result = action();
+                StatusCodeResult? statusCodeResult = result as StatusCodeResult;
+                Assert.IsNotNull( statusCodeResult );
+
+                Assert.AreEqual( (int)HttpStatusCode.Forbidden, statusCodeResult.StatusCode );
+            }
+
+            config.AllowedAdminHosts = ["somewhere.shendrick.net"];
+
+            foreach( string url in urlsToTry )
+            {
+                model.Request.Host = new HostString( url );
+
+                IActionResult result = action();
+                StatusCodeResult? statusCodeResult = result as StatusCodeResult;
+                Assert.IsNotNull( statusCodeResult );
+
+                Assert.AreEqual( (int)HttpStatusCode.Forbidden, statusCodeResult.StatusCode );
+            }
+        }
+    }
+}
---
+++ src/Jaller.Tests/Pages/Admin/IndexTests.cs
@@ -0,0 +1,75 @@
+﻿//
+// Jaller - An advanced IPFS Gateway
+// Copyright (C) 2025 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Jaller.Core.Configuration;
+using Jaller.Server.Pages.Admin;
+using Jaller.Standard;
+using Moq;
+
+namespace Jaller.Tests.Pages.Admin;
+
+[TestClass]
+public sealed class IndexTests
+{
+    // ---------------- Fields ----------------
+
+    private Mock<IJallerCore>? core;
+
+    // ---------------- Setup / Teardown ----------------
+
+    [TestInitialize]
+    public void TestSetup()
+    {
+        this.core = new Mock<IJallerCore>( MockBehavior.Strict );
+    }
+
+    [TestCleanup]
+    public void TestTeardown()
+    {
+    }
+
+    // ---------------- Properties ----------------
+
+    private Mock<IJallerCore> Core
+    {
+        get
+        {
+            ArgumentNullException.ThrowIfNull( this.core );
+            return this.core;
+        }
+    }
+
+    // ---------------- Tests ----------------
+
+    [TestMethod]
+    public void HostRestrictionTest()
+    {
+        // Setup
+        var config = new JallerConfig();
+        this.Core.Setup( m => m.Config ).Returns( config );
+
+        var uut = new IndexModel( this.Core.Object );
+
+        // Act / Check
+        CommonAdminUnitTests.DoHostRestrictionTest(
+            () => uut.OnGetAsync().Result,
+            uut,
+            config.Web
+        );
+    }
+}
