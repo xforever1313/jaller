2f7a798cd062d40730d9413f61a5cfb924b259f6
Seth Hendrick <seth@shendrick.net>
2025-04-12 22:57:22 -0400

    Added helper methods

--- src/Jaller.Server/Models/BasePageModel.cs
+++ src/Jaller.Server/Models/BasePageModel.cs
@@ -19,6 +19,7 @@
 using Jaller.Contracts.About;
 using Jaller.Server.Extensions;
 using Jaller.Standard;
+using Jaller.Standard.FileManagement;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Jaller.Server.Models
@@ -42,6 +43,98 @@ namespace Jaller.Server.Models
 
         public JallerVersionInfo JallerVersionInfo { get; }
 
-        public bool AllowAdminAccess => this.core.Config.Web.IsAdminRequstAllowed( this.Request );
+        public DownloadPolicy UserDownloadPolicy
+        {
+            get
+            {
+                if( this.User is null )
+                {
+                    return DownloadPolicy.Public;
+                }
+                else if( this.User.IsInRole( Roles.User.GetRoleName() ) )
+                {
+                    // User must be logged in to download private files.
+                    return DownloadPolicy.Private;
+                }
+                else
+                {
+                    return DownloadPolicy.Public;
+                }
+            }
+        }
+
+        public MetadataPolicy UserMetadataPolicy
+        {
+            get
+            {
+                if( this.User is null )
+                {
+                    return MetadataPolicy.Public;
+                }
+                else if( this.User.IsInRole( Roles.User.GetRoleName() ) )
+                {
+                    // User must be logged in to see private files.
+                    return MetadataPolicy.Private;
+                }
+                else
+                {
+                    return MetadataPolicy.Public;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the logged in user can edit metadata.
+        /// </summary>
+        public bool AllowMetadataEdit
+        {
+            get
+            {
+                if( this.User is null )
+                {
+                    return false;
+                }
+
+                return this.User.IsInRole( Roles.Editor.GetRoleName() );
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the logged in user can upload files.
+        /// The user must have permission, and Jaller must be connected to Kubo.
+        /// </summary>
+        public bool AllowFileUpload
+        {
+            get
+            {
+                if( this.User is null )
+                {
+                    return false;
+                }
+                else if( this.core.Config.Ipfs.KuboUrl is null )
+                {
+                    return false;
+                }
+
+                return this.User.IsInRole( Roles.Uploader.GetRoleName() );
+            }
+        }
+
+        public bool AllowAdminAccess
+        {
+            get
+            {
+                if( this.User is null )
+                {
+                    return false;
+                }
+                else if( this.core.Config.Web.IsAdminRequstAllowed( this.Request ) == false )
+                {
+                    return false;
+                }
+                
+                return this.User.IsInRole( Roles.Admin.GetRoleName() );
+            }
+        }
     }
 }
